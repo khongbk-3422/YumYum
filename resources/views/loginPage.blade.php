318f39da2357ea7b2e4d10a2f921d839d3c988e7
TYouJie <yiling13579@gmail.com>
2023-06-14 19:50:38 +0800

    updatelogin

--- resources/views/loginPage.blade.php
+++ resources/views/loginPage.blade.php
@@ -7,8 +7,7 @@
     <title>Login</title>
     <script src="https://kit.fontawesome.com/84db3d8316.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
-    
-    <script src="('js/script.js')"></script>
+ 
     <style>
         *{
             font-family: 'Raleway', sans-serif;
@@ -292,4 +291,17 @@
     </form>
     -->
 </body>
+<script>
+    const loginPanel = document.querySelector('.loginPanel');
+const login_link = document.querySelector('.loginLink');
+const register_link = document.querySelector('.registerLink');
+
+register_link.addEventListener('click', () => {
+    loginPanel.classList.add('active');
+});
+
+login_link.addEventListener('click', () => {
+    loginPanel.classList.remove('active');
+});
+</script>
 </html>
