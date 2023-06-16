8e1f5f262b6af740d9714e11bde26165ce77d1af
TYouJie <yiling13579@gmail.com>
2023-06-16 11:38:44 +0800

    update login, footer, header, restPage

---
+++ resources/views/cdn.blade.php
@@ -0,0 +1,14 @@
+<!-- STYLE -->
+<!-- bootstrap 5-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<!-- bootstrap ICON -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+<!-- google font-->
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
+
+
+<!--SCRIPT -->
+<!-- font awesome icon-->
+<script src="https://kit.fontawesome.com/84db3d8316.js" crossorigin="anonymous"></script>
+<!-- bootstrap js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
