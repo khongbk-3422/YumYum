8daa2a083dd8c4acde99cc7858c3f3699cd24f96
TYouJie <yiling13579@gmail.com>
2023-06-19 22:36:46 +0800

    update restDetailsPage

--- resources/views/restaurantDetailsPage.blade.php
+++ resources/views/restaurantDetailsPage.blade.php
@@ -8,22 +8,213 @@
     <title>Document</title>
     @include('cdn')
     <style>
+        *{
+            font-family: 'Raleway', sans-serif;
+        }
+
         .bodycontainer{
-            margin-top:65px;
-            background:pink;
+            padding:10px;
+            margin-top:80px;
+        }
+
+        .restDetails .restName{
+            margin:10px 15px;
+            line-height:2;
+        }
+
+        .restDetails .restinfo{
+            margin-left:12px;
+        }
+
+        .restDetails i{
+            font: size 14px;
+            padding:10px;
+        }
+
+        .cardcontainer{
+            padding:20px;
+        }
+
+        .cardcontainer .card{
+            height:300px;
+        }
+
+        .cardcontainer .card .card-body p{
+            font-size:14px;
+            letter-spacing:2px;
+        }
+
+        /* Ratings and reviews */
+        .ratingcontainer{
+            display:flex;
+            flex-direction:column;
+            justify-content:center;
+            align-items:center;
+        }
+
+        .ratingcontainer .title{
+            font-size:30px;
+            padding:10px 10px;
+        }
+
+        .reviewcontainer{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            flex-wrap:wrap;
+            width:100%;
+        }
+
+        .reviewbox{
+            width:500px;
+            box-shadow:0 5px 5px rgba(0, 0, 0, 0.3);
+            background-color:#f2f2f0;
+            padding:15px;
+            margin:15px;
+            cursor:pointer;
+            border-radius:8px;
+        }
+
+        .profile{
+            display:flex;
+            align-items:center;
+        }
+
+        .profile .custpic{
+            width:50px;
+            height:50px;
+            border-radius:50%;
+            overflow:hidden;
+            margin-right:10px;
+        }
+
+        .profile .custpic img{
+            width:100%;
+            height:100%;
+            object-fit:cover;
+            object-position:center;
+        }
+
+        .custname{
+            display:flex;
+            flex-direction:column;
+        }
+
+        .box-top{
+            display:flex;
+            justify-content:space-between;
+            align-items:center;
+            margin-bottom:20px;
+        }
+
+        .rating i{
+            color:yellow;
+        }
+
+        .custreview .date{
+            color:grey;
+        }
+
+        .custreview p{
+            font-size:14px;
+            color:black;
         }
     </style>
 </head>
 <body>
     <div class="bodycontainer">
+        <!-- get from rest_table -->
         <div class="restDetails">
             <div class="restName">
                 <h3>Iketeru</h3>
+                <i class="fa-solid fa-location-dot"><span class="restinfo">addresss</span></i> |
+                <i class="fa-solid fa-square-phone"><span class=restinfo>01123455</span></i>
             </div>
         </div>
+        
 
+        <div class="menuImage">
+            <!-- image -->
+        </div>
+
+        <div class="cardcontainer">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Price Range</h5>
+                            <p>RM20-RM500</p>
+
+                            <h5>Cuisine</h5>
+                            <p>Japanese</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Price Range</h5>
+                            <p>RM20-RM500</p>
+
+                            <h5>Cuisine</h5>
+                            <p>Japanese</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Location and Contact</h5>
+                            <!-- Display map -->
+                            <p>map</p>
+                            <p>Address0</p>
+
+                            <h5>Contact</h5>
+                            <p>111111111</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- get from rating_table -->
+        <div class="ratingcontainer">
+            <div>
+                <h5 class="title">Reviews and Ratings</h5>
+            </div>
+            
+            <div class="reviewcontainer">
+                <div class="reviewbox">
+                    <div class="box-top">
+                        <div class="profile">
+                            <div class="custpic">
+                                <img src="{{asset('image/rest1.jpg')}}" alt="">
+                            </div>
+
+                            <div class="custname">
+                                <strong>alicia</strong>
+                            </div>
+                        </div>
+
+                        <div class="rating">
+                            <!-- how to display number star based on sql -->
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+
+                    <div class="custreview">
+                        <p class="date">2023/05/12 21:45:21</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed fuga praesentium repellendus, ad quas quasi consequuntur laudantium nam eius dolore neque esse fugiat iste debitis recusandae placeat commodi distinctio cumque!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    
 </body>
 </html>
 
