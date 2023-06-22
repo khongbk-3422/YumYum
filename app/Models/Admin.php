f16f1b1aa1da69ef7e07ffb0e01f4962c5c6a9b6
khongbk-3422 <khongbk030422@gmail.com>
2023-06-22 14:05:09 +0800

    login

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admins extends Model
+class Admin extends Model
 {
     protected $keyType = 'string';  //set able to be varchar in primary key
     use HasFactory;
