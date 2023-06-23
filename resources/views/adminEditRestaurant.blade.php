c32fcd207cc208c872a4183fe764e5fe0cef2014
Omnislasher03 <123357072+Omnislasher03@users.noreply.github.com>
2023-06-23 09:55:55 +0800

    editAdminRestaurant

---
+++ resources/views/adminEditRestaurant.blade.php
@@ -0,0 +1,42 @@
+@include('adminHeader')
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @include('cdn')
+
+    <style>
+        .tablecontainer{
+            background: pink;
+            margin-top:80px;
+        }
+    </style>
+</head>
+<body>
+    <div class="tablecontainer">
+        <table class="table table-dark">
+            <thead>
+              ...
+            </thead>
+            <tbody>
+              <tr class="table-active">
+                ...
+              </tr>
+              <tr>
+                ...
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2" class="table-active">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+    </div>
+    
+</body>
+</html>
