ac56404318de8f939ba14495392e5dd98aecd45e
TYouJie <yiling13579@gmail.com>
2023-06-24 17:47:03 +0800

    update custHistoryPage

--- resources/views/custHistoryPage.blade.php
+++ resources/views/custHistoryPage.blade.php
@@ -12,11 +12,62 @@
         *{
             font-family: 'Raleway', sans-serif;
         }
+
+        .maincontainer{
+            margin-top:80px;
+            /* background:pink; */
+            /* height:110vh; */
+        }
+
+        .historycontainer{
+            width:calc(100% - 260px);
+            /* background:purple; */
+            margin:auto;
+        }
+
+        .historycontainer .title{
+            text-align:center;
+            padding:10px;
+        }
+
+        .historyperday .date, .history{
+            margin:12px;
+        }
+
+        .history .data h4{
+            font-size:16px;
+            letter-spacing:3px;
+        }
+
+        .historyperday .data span{
+            margin:20px;
+        }
+
+
     </style>
 </head>
 <body>
     <div class="maincontainer">
+        <div class="historycontainer">
+            <div class="title">
+                <h2>History</h2>
+            </div>
+<!-- loop from database and order by date -->
+            <div class="historyperday">
+                <div class="date">
+                    <h4>Date</h4>
+                </div>
 
+                <div class="history">
+                    <div class="data">
+                        <h4>08:10:22 <span>Prime</span></h4>
+                        <h4>08:10:22 <span>Prime</span></h4>
+                        <h4>08:10:22 <span>Prime</span></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
     </div>
 </body>
 </html>
