15dc5a52fac3b6cd41f57e5e5af1442e285cb2c8
NHCh1 <134712763+NHCh1@users.noreply.github.com>
2023-06-29 21:45:50 +0800

    .

--- resources/views/adminHomePage.blade.php
+++ resources/views/adminHomePage.blade.php
@@ -362,7 +362,7 @@
                                         </div>
                                     </div>
                                     <div class="action">
-                                        <a href="#" onclick="editCustomer({{ $cust_list['cust_id'] }})">
+                                        <a href="/adminEditCustomer?customerId={{ $cust_list['cust_id'] }} ">
                                             <span class="fa-solid fa-magnifying-glass"></span>
                                         </a>
                                     </div>
@@ -377,9 +377,6 @@
     </main>
 </body>
 <script>
-    function editCustomer(custId) {
-        sessionStorage.setItem('customerId', custId);
-        window.location.href = '/adminEditCustomer';
-    }
+
 </script>
 </html>
