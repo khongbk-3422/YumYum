418ad680cff9251b592b1f90c3e85cfe3186414b
TYouJie <yiling13579@gmail.com>
2023-07-02 09:11:50 +0800

    test swal.blade.php

--- resources/views/cdn.blade.php
+++ resources/views/cdn.blade.php
@@ -22,3 +22,7 @@
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- sweet alert -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
